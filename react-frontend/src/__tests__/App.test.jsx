cc6a953b6fdb797a4ac25c0a79f4e1994edb2304
David Charlton <david.charlton@ingka.ikea.com>
2024-10-08 11:10:22 -0400

    test update

--- react-frontend/src/__tests__/App.test.jsx
+++ react-frontend/src/__tests__/App.test.jsx
@@ -2,4 +2,10 @@ import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
 import App from "../App";
 
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ test: 100 }),
+  }),
+)
+
 test("renders an h1 tag", () => {});
